0332bcca43b43d113d2a2084ff2541171c32e2f1
Pilar Leminski Veiga <pilar.leminski.veiga@avanade.com>
2022-10-10 16:10:33 -0300

    'Deploy'

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,102 @@
-import React from 'react'
+import React from 'react';
+import styles from './Header.module.css';
 
-export default function Header() {
+/*
+Existem 5 maneiras diferentes de utilizar CSS no React:
+1. CSS puro (não recomendado) - Ex. arquivo.css
+2. CSS Modules - Ex. arquivo.module.css
+3. Sass
+4. Styled Components - Ex. arquivo.tsx
+5. Emotion - pode usar a função css ou o componente styled
+*/
+import { css } from '@emotion/css';
+import styled from '@emotion/styled';
+import Link from 'next/link';
+
+//styled component
+const HeaderPage = styled.header`
+width: 100%;
+height: 100px;
+background: #ddd;
+display: flex;
+align-items: center;
+justify-content: space-between;
+& > div:first-child,
+& > div:last-child {
+  margin: 0 5%;
+}`
+;
+
+const NavMenu = styled.nav`
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  height: 50px;
+  & a:hover {
+    font-weight: bold;
+  }
+  & li {
+    margin-top: 4px;
+    border-bottom: 4px solid transparent;
+    padding-bottom: 20px;
+  }
+  & li:hover {
+    border-bottom: double 4px orange;
+  }
+`;
+
+const InputMenu = styled.input`
+  border: none;
+  border-radius: 10px;
+  padding: 2px 10px;
+  height: 25px;
+`;
+
+const ButtonMenu = styled.button`
+  border: none;
+  border-radius: 10px;
+  margin-left: 5px;
+  color: #333;
+  background: linear-gradient(150deg, #ffeb84 0%, #ffd53e 100%);
+  box-shadow: 0px 2px 5px -1px rgb(59, 59, 59, 0.5);
+
+`;
+
+type HeaderProps = {
+  pesquisar?: string;
+  menu?: Array<string>;
+}
+
+export default function Header(props:HeaderProps) {
   return (
-    <header>
+//    <header className={styles['menu-site']}>
+      <HeaderPage>
         <div>Logotipo</div>
-        <nav>Navegação Menu</nav>
-        <div>Caixa de busca</div>
-    </header>
+        <NavMenu>
+          <ul className={ css`
+          margin: 0;
+          padding: 0;
+          display: inline-flex;
+          & > li {
+            list-style: none;
+            min-width: 100px;
+            text-align: center;
+            padding: 10px;
+          }`}>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/login">Login</Link></li>
+            <li><Link href="/sobre">Sobre</Link></li>
+            <li><Link href="#">Galeria</Link></li>
+            <li><Link href="#">Serviços</Link></li>
+            <li><Link href="#">Blog</Link></li>
+            <li><Link href="#">Contato</Link></li>
+          </ul>
+        </NavMenu>
+        <div>
+          <InputMenu></InputMenu>
+          <ButtonMenu>{props.pesquisar?props.pesquisar:"Search"}</ButtonMenu>
+        </div>
+      </HeaderPage>
+//    </header>
   )
 }
